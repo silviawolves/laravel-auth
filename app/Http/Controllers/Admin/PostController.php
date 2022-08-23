7dc28d7d68646bb79399931a456f886f34bd9294
silviawolves <silviacanepa.w@gmail.com>
2022-08-23 17:41:09 +0200

    post

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -3,10 +3,47 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class PostController extends Controller
 {
+    private function findBySlug($slug)
+    {
+        $post = Post::where('slug', $slug)->first();
+
+        if (!$post) {
+            abort(404);
+        }
+
+        return $post;
+    }
+
+    private function generateSlug($text)
+    {
+        $toReturn = null;
+        $counter = 0;
+
+        do {
+            $slug = Str::slug($text);
+
+            if ($counter > 0) {
+                $slug .= "-" . $counter;
+            }
+
+            $slug_esiste = Post::where('slug', $slug)->first();
+
+            if ($slug_esiste) {
+                $counter++;
+            } else {
+                $toReturn = $slug;
+            }
+        } while ($slug_esiste);
+
+        return $toReturn;
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -14,7 +51,8 @@ class PostController extends Controller
      */
     public function index()
     {
-        //
+        $posts = Post::orderBy('created_at', 'desc')->get();
+        return view('admin.posts.index', compact('posts'));
     }
 
     /**
@@ -24,7 +62,7 @@ class PostController extends Controller
      */
     public function create()
     {
-        //
+        return view('admin.posts.create');
     }
 
     /**
@@ -35,7 +73,16 @@ class PostController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validatedData = $request->validate([
+            'title' => 'required|min:10',
+            'content' => 'required|min:10'
+        ]);
+
+        $post = new Post();
+        $post->fill($validatedData);
+        $post->slug = $this->generateSlug($post->title);
+        $post->save();
+        return redirect()->route('admin.posts.show', $post->slug);
     }
 
     /**
@@ -44,9 +91,10 @@ class PostController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show($slug)
     {
-        //
+        $post = $this->findBySlug($slug);
+        return view('admin.posts.show', compact('post'));
     }
 
     /**
@@ -55,9 +103,11 @@ class PostController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit($slug)
     {
-        //
+        $post = $this->findBySlug($slug);
+
+        return view('admin.posts.edit', compact('post'));
     }
 
     /**
@@ -67,9 +117,21 @@ class PostController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, $slug)
     {
-        //
+        $validatedData = $request->validate([
+            'title' => 'required|min:10',
+            'content' => 'required|min:10'
+        ]);
+        $post = $this->findBySlug($slug);
+
+        if ($validatedData['title'] !== $post->title) {
+            $post->slug = $this->generateSlug($validatedData['title']);
+        }
+
+        $post->update($validatedData);
+
+        return redirect()->route('admin.posts.show', $post->slug);
     }
 
     /**
@@ -78,8 +140,10 @@ class PostController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy($slug)
     {
-        //
+        $post = $this->findBySlug($slug);
+        $post->delete();
+        return redirect()->route('admin.posts.index');
     }
 }
